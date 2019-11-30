b549ff1877cb18183ba3bd2637a9a55b5ff38745
Paulo Raphael <tubande@gmail.com>
2019-11-30 11:20:12 -0300

    Switching Player each turn

--- src/application/UI.java
+++ src/application/UI.java
@@ -3,9 +3,11 @@ package application;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
+import sun.swing.PrintingStatus;
 
 public class UI {
 
@@ -31,7 +33,8 @@ public class UI {
 	public static void clearScreen() {
 		System.out.print("\033[H\033[2J");
 		System.out.flush();
-	}	
+	}
+
 	public static ChessPosition readChessPOsition(Scanner sc) {
 		try {
 			String s = sc.nextLine();
@@ -41,6 +44,13 @@ public class UI {
 		} catch (RuntimeException e) {
 			throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8.");
 		}
+	}
+
+	public static void printMatch(ChessMatch chessMatch) {
+		printBoard(chessMatch.getPieces());
+		System.out.println();
+		System.out.println("Turn : " + chessMatch.getTurn());
+		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
 	}
 
 	public static void printBoard(ChessPiece[][] pieces) {
@@ -66,7 +76,7 @@ public class UI {
 	}
 
 	private static void printPiece(ChessPiece piece, boolean background) {
-		if(background) {
+		if (background) {
 			System.out.print(ANSI_BLUE_BACKGROUND);
 		}
 		if (piece == null) {
